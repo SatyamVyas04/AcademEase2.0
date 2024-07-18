95456642d0951da054125fb8e90bcf795fb567c0
nishakp3005 <121110503+nishakp3005@users.noreply.github.com>
2024-07-18 19:19:52 +0530

    setup page added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,25 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Login from './pages/Login/Login.jsx';
-import HomePage from './pages/HomePage/HomePage.jsx';
-import { GoogleOAuthProvider } from '@react-oauth/google';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./pages/Login/Login.jsx";
+import HomePage from "./pages/HomePage/HomePage.jsx";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import SetupPage from "./pages/SetupPage/SetupPage.jsx";
 
 const clientIdAuth = import.meta.env.VITE_AUTH0_CLIENT_ID;
 
 function App() {
-  return (
-    <GoogleOAuthProvider clientId={clientIdAuth}>
-      <Router>
-        <div className="App">
-          <Routes>
-            <Route path="/" element={<Login />} />
-            <Route path="/home" element={<HomePage />} />
-          </Routes>
-        </div>
-      </Router>
-    </GoogleOAuthProvider>
-  );
+    return (
+        <GoogleOAuthProvider clientId={clientIdAuth}>
+            <Router>
+                <div className="App">
+                    <Routes>
+                        <Route path="/" element={<Login />} />
+                        <Route path="/home" element={<HomePage />} />
+                        <Route path="/setup" element={<SetupPage/>} />
+                    </Routes>
+                </div>
+            </Router>
+        </GoogleOAuthProvider>
+    );
 }
 
 export default App;
