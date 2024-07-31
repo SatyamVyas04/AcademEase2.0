6d5d62b0a11623c7e6d7277c5d9f2bd9c2fed35f
Nishita Panchal <nishakp3005@gmail.com>
2024-07-31 23:15:24 +0530

    minor changes

--- client/src/pages/pfp/ProfilePage.jsx
+++ client/src/pages/pfp/ProfilePage.jsx
@@ -1,12 +1,12 @@
 import ProfileCard from "@/components/ProfileCard";
-import ProfileLeftNavbar from "@/components/ProfileLeftNavbar";
 import { SkillsSection } from "@/components/SkillsSection";
+import Sidebar from "@/components/SideNav";
 
 function ProfilePage() {
     return (
-        <div className="flex flex-row justify-between items-center border-2 border-gray-200 p-6">
+        <div className="flex flex-row justify-between border-2 border-gray-200 p-6">
             <div className="basis-1/4 border-2 border-gray-200 rounded-xl max-w-[250px]">
-                <ProfileLeftNavbar />
+                <Sidebar />
             </div>
             <div className="basis-2/4 border-2 border-gray-200 rounded-xl">
                 <SkillsSection />
