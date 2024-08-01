81d95f9cea88159b9c13141f4c17d4f56bc55c8b
Nishita Panchal <nishakp3005@gmail.com>
2024-08-01 10:57:25 +0530

    some changes in skills and profile page

--- client/src/pages/pfp/ProfilePage.jsx
+++ client/src/pages/pfp/ProfilePage.jsx
@@ -4,15 +4,17 @@ import Sidebar from "@/components/SideNav";
 
 function ProfilePage() {
     return (
-        <div className="flex flex-row justify-between border-2 border-gray-200 p-6">
-            <div className="basis-1/4 border-2 border-gray-200 rounded-xl max-w-[250px]">
-                <Sidebar />
-            </div>
-            <div className="basis-2/4 border-2 border-gray-200 rounded-xl">
+        <div className="flex flex-row justify-evenly items-start col-span-10 gap-8">
+            <div className="border-2 bg-white border-gray-200 rounded-xl shadow-lg">
                 <SkillsSection />
             </div>
-            <div className="basis-1/2 border-2 border-gray-200 rounded-xl max-w-[350px]">
-                <ProfileCard />
+            <div className="flex flex-col gap-4 max-w-[350px]">
+                <div className="border-2 bg-white border-gray-200 rounded-xl shadow-lg">
+                    <ProfileCard />
+                </div>
+                <button className="h-10 text-primary-foreground bg-primary border-2 rounded-xl shadow-lg">
+                    Edit Profile
+                </button>
             </div>
         </div>
     );
